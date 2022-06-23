57526fef5546a67b643b438b1ee36d8542b647ec
hhack <hans.f.hack@googlemail.com>
2022-06-23 14:02:45 +0200

    target blank

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,8 +2,8 @@
 	<h1 class="text-2xl sm:text-4xl mb-5 font-bold">Contact</h1>
 	<div class="sm:text-xl">bold geo</div>
 	<div class="sm:text-xl">
-		<a class="underline" href="https://hanshack.com/">Hans Hack</a> &
-		<a class="underline" href="https://alsino.io/">Alsino Skowronnek</a>
+		<a class="underline" target="_blank" href="https://hanshack.com/">Hans Hack</a> &
+		<a class="underline" target="_blank" href="https://alsino.io/">Alsino Skowronnek</a>
 	</div>
 	<div class="sm:text-xl">Wassertorstraße 65</div>
 	<div class="sm:text-xl">10969 Berlin</div>
