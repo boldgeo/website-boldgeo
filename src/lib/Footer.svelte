94372ec661cb94649361f5be3c4603d10d891d2d
alsino <iamahorse@gmail.com>
2025-08-06 14:30:13 +0200

    adited footer information

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,11 +2,10 @@
 	<h1 class="text-2xl sm:text-4xl mb-5 font-bold">Contact</h1>
 	<div class="sm:text-xl">bold geo</div>
 	<div class="sm:text-xl">
-		<a class="underline" target="_blank" href="https://hanshack.com/">Hans Hack</a> &
 		<a class="underline" target="_blank" href="https://alsino.io/">Alsino Skowronnek</a>
 	</div>
-	<div class="sm:text-xl">Wassertorstraße 65</div>
-	<div class="sm:text-xl">10969 Berlin</div>
+	<div class="sm:text-xl">Seumestrasse 8</div>
+	<div class="sm:text-xl">10245 Berlin</div>
 	<div class="sm:text-xl">Germany</div>
 	<div class="pt-10"><a class="sm:text-xl" href="mailto:info@boldgeo.de">info@boldgeo.de</a></div>
 </footer>
